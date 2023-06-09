7945d2dfae7bbbcf33ed03a5f677840ca78ab396
Matsum-e <131111812+Matsum-e@users.noreply.github.com>
2023-06-09 02:03:53 -0300

    teste
    talvez vá

---
+++ assets/js/acordeon.js
@@ -0,0 +1,19 @@
+
+const acordeontrigo = document.querySelectorAll('.acordeon .trigger');
+
+acordeontrigo.forEach((trigger)=>{
+    trigger.addEventListener('click', (e)=>{
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
+        if(isOpen){
+            acordeon.classList.remove('open')
+             acordeon.classList = Array.of(acordeon.classList).filter(className => className !== 'open ')
+        }else{
+            acordeon.classList.add('open')
+        }
+
+
+
+    })
+})
